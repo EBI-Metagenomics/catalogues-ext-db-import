65219200112e717e2e129d8b9e61b193b3941a64
Tatiana Gurbich <tgurbich@gmail.com>
2025-12-12 09:41:15 +0000

    Added the genome type column to the metadata table

--- helpers/database_import_scripts/uniprot/generate_uniprot_metadata.py
+++ helpers/database_import_scripts/uniprot/generate_uniprot_metadata.py
@@ -10,16 +10,16 @@ from retry import retry
 def main(metadata_file, outfile, processed_taxonomy):
     original_df = pd.read_csv(metadata_file, sep='\t')
     filtered_df = original_df[original_df['Genome'] == original_df['Species_rep']]
-    relevant_columns = ['Genome', 'N50', 'Completeness', 'Contamination', 'Sample_accession']
+    relevant_columns = ['Genome', 'N50', 'Completeness', 'Contamination', 'Genome_type']
     reduced_df = filtered_df[relevant_columns].copy()
     reduced_df['GCA_accession'] = reduced_df['Genome'].apply(get_gca_accession, processed_taxonomy=processed_taxonomy)
     reduced_df['Taxid'] = reduced_df['Genome'].apply(get_taxid, processed_taxonomy=processed_taxonomy)
     reduced_df['Species_level'] = reduced_df['Genome'].apply(get_species_level, processed_taxonomy=processed_taxonomy)
-    reduced_df = reduced_df.drop(columns=["Sample_accession"])
 
     # Reorder the columns
     columns = reduced_df.columns.tolist()  # Convert columns to a list
-    columns.insert(1, columns.pop(4))  # Move the 5th column to the 2nd position
+    columns.insert(1, columns.pop(5))  # Move the 6th column to the 2nd position (GCA_accession)
+    columns.append(columns.pop(columns.index("Genome_type")))  # make Genome_type the last column
     reordered_df = reduced_df[columns]  # Create a new DataFrame with reordered columns
     reordered_df.to_csv(outfile, index=False, sep='\t')
 
